0d4e18c6c463e109cbff71cdb41da7807af3c494
Arin Sharma <arinsharma.global@gmail.com>
2025-09-25 03:18:49 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
-export const serverUrl = "http://localhost:3000";
+export const serverUrl = "https://gigglechat-7q57.onrender.com";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
