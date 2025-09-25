05b9f7a36c1d345aef8054b77edc2a4040a37a37
Arin Sharma <arinsharma.global@gmail.com>
2025-09-25 12:27:04 +0530

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -70,7 +70,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      sameSite: "Strict",
+      sameSite: "None",
       secure: true,
     });
 
