5c2f68f48197ddbddad7cb55f2eff449e16f8f23
Arin Sharma <arinsharma.global@gmail.com>
2025-09-25 13:14:06 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,14 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig(({ mode }) => {
-  const isProduction = mode === 'production';
-
-  return {
-    plugins: [react(), tailwindcss()],
-    server: {
-      host: '0.0.0.0',
-      allowedHosts: isProduction ? [process.env.RENDER_EXTERNAL_HOSTNAME] : [],
-    },
-  };
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: "0.0.0.0",
+    allowedHosts: [process.env.RENDER_EXTERNAL_HOSTNAME],
+  },
 });
